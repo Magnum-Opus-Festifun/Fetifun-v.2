c0ff3d6aa8cd2cd4ea032c4a9319b308e18748a8
Karlen404 <karlenderawan07@gmail.com>
2023-12-16 20:08:25 +0800

    login regist done

--- src/pages/Login/Components/loginright.jsx
+++ src/pages/Login/Components/loginright.jsx
@@ -1,12 +1,92 @@
+// Import statements
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Envelope, LockFill } from 'react-bootstrap-icons';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-import { Button, Form } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-
-import { Person, LockFill, Google, Facebook, Apple } from "react-bootstrap-icons";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+// Login component
 function Login() {
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [loginStatus, setLoginStatus] = useState({
+    isSuccess: null,
+    message: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    // Validate that email and password are not empty
+    if (!loginData.email || !loginData.password) {
+      setLoginStatus({
+        isSuccess: false,
+        message: 'Please fill in both email and password.',
+      });
+      return;
+    }
+  
+    try {
+      const response = await axios.post('http://localhost:4000/login', loginData);
+  
+      setLoginStatus({
+        isSuccess: true,
+        message: response.data.message,
+      });
+  
+      // Redirect ke halaman sesuai peran
+      const userRole = response.data.data.role;
+      if (userRole === 'user') {
+        navigate('/user-page');
+      } else if (userRole === 'eo') {
+        navigate('/eo-page');
+      }
+    } catch (error) {
+      if (error.response) {
+        const status = error.response.status;
+        if (status === 401) {
+          setLoginStatus({
+            isSuccess: false,
+            message: 'Email atau password salah.',
+          });
+        } else if (status === 404) {
+          setLoginStatus({
+            isSuccess: false,
+            message: 'Email belum terdaftar.',
+          });
+        } else {
+          setLoginStatus({
+            isSuccess: false,
+            message: 'Terjadi kesalahan. Silakan coba lagi.',
+          });
+        }
+      } else if (error.request) {
+        setLoginStatus({
+          isSuccess: false,
+          message: 'Tidak ada respons dari server. Silakan coba lagi.',
+        });
+      } else {
+        setLoginStatus({
+          isSuccess: false,
+          message: 'Terjadi kesalahan. Silakan coba lagi.',
+        });
+      }
+    }
+  };
+  
+
+  
+
   return (
     <>
       <img
@@ -15,37 +95,50 @@ function Login() {
         style={{ marginLeft: "30%", marginTop: "30%", marginBottom: "0%" }}
         alt="Navbar Logo"
       />
-      <Form style={{ width: "80%", height: "100vh", marginLeft: "10%", marginTop: "10%" }}>
+      <Form style={{ width: "80%", height: "100vh", marginLeft: "10%", marginTop: "10%" }} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label><Person /> Email</Form.Label>
-          <Form.Control type="email" placeholder="Email/username" />
+          <Form.Label><Envelope /> Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Email/username"
+            name="email"
+            value={loginData.email}
+            onChange={handleChange}
+          />
         </Form.Group>
-
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label><LockFill />Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+          />
         </Form.Group>
         <div className="d-grid gap-2">
           <Button variant="primary" size="lg" type="submit">
             Login
           </Button>
         </div><br />
+        
+        {loginStatus.isSuccess !== null && (
+          <Alert variant={loginStatus.isSuccess ? 'success' : 'danger'} className="mt-3">
+            {loginStatus.message}
+          </Alert>
+        )}
+
         <p>Belum punya akun? <Link to='/register'>Register</Link></p>
-        <div className="mt-5">
+        {/* <div className="mt-5">
           <p>Atau lanjutkan melalui:</p>
           <div className="ms-auto mt-5" >
-            <Row className="ms-5">
-              <Col><Button variant="primary"><Google size={35} /></Button>{' '}  </Col>
-              <Col><Button variant="primary"><Facebook size={35} /></Button>{' '}</Col>
-              <Col><Button variant="primary"><Apple size={35} /></Button>{' '}</Col>
-            </Row>
+           
           </div>
-        </div>
+        </div> */}
       </Form>
     </>
-
-  )
+  );
 }
 
 export default Login;
--- src/pages/Register/Components/regisright.jsx
+++ src/pages/Register/Components/regisright.jsx
@@ -1,11 +1,67 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Person, LockFill, Envelope } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
-import { Button, Form } from "react-bootstrap";
-import { Person, LockFill, Google, Facebook, Apple, Telephone, Envelope } from "react-bootstrap-icons";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
+import axios from 'axios';
 
 function Regisright() {
+  const [formData, setFormData] = useState({
+    name: '',
+    role: '',
+    email: '',
+    password: '',
+  });
+  const [registrationStatus, setRegistrationStatus] = useState({
+    isSuccess: null,
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    // Memeriksa apakah role sudah dipilih
+    if (!formData.role) {
+      setRegistrationStatus({
+        isSuccess: false,
+        message: 'Harap pilih peran (role) sebelum mendaftar.',
+      });
+      return;
+    }
+  
+    try {
+      // Kirim data formulir ke API
+      const response = await axios.post('http://localhost:4000/users', formData);
+  
+      // Proses respons dari API
+      setRegistrationStatus({
+        isSuccess: true,
+        message: 'Registrasi berhasil! Silakan login.',
+      });
+    } catch (error) {
+      if (error.response?.status === 409) {
+        // Konflik: email sudah terdaftar
+        setRegistrationStatus({
+          isSuccess: false,
+          message: 'Email sudah terdaftar. Gunakan email lain.',
+        });
+      } else {
+        // Tangani kesalahan lain
+        const errorMessage = error.response?.data?.message || 'Terjadi kesalahan. Silakan coba lagi.';
+        setRegistrationStatus({
+          isSuccess: false,
+          message: errorMessage,
+        });
+      }
+    }
+  };
+  
+  
+
   return (
     <div>
       <img
@@ -14,16 +70,27 @@ function Regisright() {
         style={{ marginLeft: "30%", marginTop: "5%", marginBottom: "5%" }}
         alt="Navbar Logo"
       />
-      <Form style={{ width: "80%", marginLeft: "5%", marginTop: "5%" }}>
+      <Form style={{ width: "80%", marginLeft: "5%", marginTop: "5%" }} onSubmit={handleSubmit}>
 
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label><Person />Nama</Form.Label>
-          <Form.Control type="text" placeholder="Nama Lengkap" />
+          <Form.Control
+            type="text"
+            placeholder="Nama Lengkap"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formNumber">
           <Form.Label>Role</Form.Label>
-          <Form.Select aria-label="Select role" defaultValue="">
+          <Form.Select
+            aria-label="Select role"
+            name="role"
+            value={formData.role}
+            onChange={handleChange}
+          >
             <option value="" disabled>Pilih Role</option>
             <option value="user">User</option>
             <option value="eo">EO (Event Organizer)</option>
@@ -32,35 +99,48 @@ function Regisright() {
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label><Envelope />Email</Form.Label>
-          <Form.Control type="email" placeholder="Email/Username" />
+          <Form.Control
+            type="email"
+            placeholder="Email/Username"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label><LockFill />Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
         </Form.Group>
 
+       
         <div className="d-grid gap-2">
           <Button variant="primary" size="lg" type="submit">
             Register
           </Button>
         </div>
+
+        {registrationStatus.isSuccess !== null && (
+          <Alert variant={registrationStatus.isSuccess ? 'success' : 'danger'} className="mt-3">
+            {registrationStatus.message}
+          </Alert>
+        )}
+        <br />
         <p>Sudah punya akun? <Link to='/login'>Login</Link></p>
-        <div className="mt-3">
+        {/* <div className="mt-3">
           <p>Atau lanjutkan melalui:</p>
           <div className="ms-5 mt-2" >
-            <Row className="ms-2">
-              <Col><Button variant="primary"><Google size={35} /></Button>{' '}  </Col>
-              <Col><Button variant="primary"><Facebook size={35} /></Button>{' '}</Col>
-              <Col><Button variant="primary"><Apple size={35} /></Button>{' '}</Col>
-            </Row>
-
 
           </div>
-        </div>
+        </div> */}
       </Form>
     </div>
-
   );
 }
 
